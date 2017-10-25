fbdf6756f2f50f67d9c9cf880e91842d54d3007c
758770768 <758770768@qq.com>
2017-10-25 18:04:33 +0800

    second

---
+++ src/org/bootrapBack/utils/ABC.java
@@ -0,0 +1,7 @@
+package org.bootrapBack.utils;
+
+public class ABC {
+     public static void main(String[] args) {
+		System.out.println(123);
+	}
+}
--- src/org/bootrapBack/utils/DBM.java
+++ src/org/bootrapBack/utils/DBM.java
@@ -8,7 +8,7 @@ public class DBM {
 	private static Connection conn = null;
 	static {
 		try {
-			Class.forName("com.mysql.jdbc.Driver");
+			        Class.forName("com.mysql.jdbc.Driver");
 			String url = "jdbc:mysql:///emp_dept_work?useUnicode=true&characterEncodig=utf8";
 			conn = DriverManager.getConnection(url, "root", "123123");
 		} catch (Exception e) {
