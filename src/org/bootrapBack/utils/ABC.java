4dc89b340a0a02655977e8980cc497eef98083a8
758770768 <758770768@qq.com>
2018-03-20 10:24:20 +0800

    abc

--- src/org/bootrapBack/utils/ABC.java
+++ src/org/bootrapBack/utils/ABC.java
@@ -4,6 +4,6 @@ public class ABC {
 	public static void main(String[] args) {
 		System.out.println(123);
 		System.out.println("thi is a good day");
-		System.out.println("but this is not a bad day");
+		System.out.println("but this is not a bad day..............");
 	}
 }
